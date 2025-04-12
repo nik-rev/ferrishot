710bd468f9efd3d576ffab0cb0232ad5b541d3a0
Nik Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-04-12 05:57:12 +0100

    chore: add winit dependency

--- flake.nix
+++ flake.nix
@@ -16,16 +16,16 @@
           inherit system;
         };
         manifest = pkgs.lib.importTOML ./Cargo.toml;
-        nativeBuildInputs = with pkgs; [ pkg-config ];
         buildInputs = with pkgs; [
           openssl
           pkg-config
           dbus
-          xorg.libxcb
-          xorg.xrandr
+
           xorg.libX11
           xorg.libXcursor
+          xorg.xrandr
           xorg.libXi
+          xorg.libxcb
           libxkbcommon
           vulkan-loader
           wayland
@@ -44,23 +44,11 @@
           cargoLock.lockFile = ./Cargo.lock;
           cargoLock.outputHashes = {
             "iced-0.14.0-dev" = "sha256-0Pe88fUGPZ6rpOM4yKXw73CMuyRfb90hFW/38SBd7JY=";
-            # "iced_core-0.14.0-dev" = "sha256-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB=";
-            # "iced_debug-0.14.0-dev" = "sha256-CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC=";
-            # "iced_futures-0.14.0-dev" = "sha256-DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD=";
-            # "iced_renderer-0.14.0-dev" = "sha256-EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE=";
-            # "iced_graphics-0.14.0-dev" = "sha256-FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF=";
-            # "iced_tiny_skia-0.14.0-dev" = "sha256-GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG=";
-            # "iced_wgpu-0.14.0-dev" = "sha256-HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH=";
             "cryoglyph-0.1.0" = "sha256-X7S9jq8wU6g1DDNEzOtP3lKWugDnpopPDBK49iWvD4o=";
-            # "iced_runtime-0.14.0-dev" = "sha256-JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ=";
-            # "iced_widget-0.14.0-dev" = "sha256-KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK=";
-            # "iced_winit-0.14.0-dev" = "sha256-LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL=";
-            # "iced_program-0.14.0-dev" = "sha256-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM=";
-            # "winit-0.30.8" = "sha256-NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN=";
             "dpi-0.1.1" = "sha256-hlVhlQ8MmIbNFNr6BM4edKdZbe+ixnPpKm819zauFLQ=";
           };
 
-          inherit nativeBuildInputs;
+          nativeBuildInputs = buildInputs;
           inherit buildInputs;
         };
       }
