163f95f128743718259ff6989094f3f123971327
Nik Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-04-12 23:26:32 +0100

    fix: do not require Display if unnecessary for ResultExt

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,10 @@
 
 /// Additional extension methods for Result
 #[easy_ext::ext(ResultExt)]
-pub impl<T, E: std::fmt::Debug + std::fmt::Display> Result<T, E> {
+pub impl<T, E: std::fmt::Debug> Result<T, E> {
     /// Like `Result::expect`, but also logs the failure
     fn log_expect(self, message: &str) -> T {
-        self.inspect_err(|err| log::error!("{message}: {err}"))
+        self.inspect_err(|err| log::error!("{message}: {err:?}"))
             .expect(message)
     }
 }
