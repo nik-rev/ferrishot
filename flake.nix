258240deca9ec14f6271438875877bcc4546350f
Nik Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-04-12 05:38:04 +0100

    chore: update nix flake

--- flake.nix
+++ flake.nix
@@ -42,6 +42,23 @@
 
           src = pkgs.lib.cleanSource ./.;
           cargoLock.lockFile = ./Cargo.lock;
+          cargoLock.outputHashes = {
+            "iced-0.14.0-dev" = "sha256-0Pe88fUGPZ6rpOM4yKXw73CMuyRfb90hFW/38SBd7JY=";
+            # "iced_core-0.14.0-dev" = "sha256-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB=";
+            # "iced_debug-0.14.0-dev" = "sha256-CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC=";
+            # "iced_futures-0.14.0-dev" = "sha256-DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD=";
+            # "iced_renderer-0.14.0-dev" = "sha256-EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE=";
+            # "iced_graphics-0.14.0-dev" = "sha256-FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF=";
+            # "iced_tiny_skia-0.14.0-dev" = "sha256-GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG=";
+            # "iced_wgpu-0.14.0-dev" = "sha256-HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH=";
+            "cryoglyph-0.1.0" = "sha256-X7S9jq8wU6g1DDNEzOtP3lKWugDnpopPDBK49iWvD4o=";
+            # "iced_runtime-0.14.0-dev" = "sha256-JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ=";
+            # "iced_widget-0.14.0-dev" = "sha256-KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK=";
+            # "iced_winit-0.14.0-dev" = "sha256-LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL=";
+            # "iced_program-0.14.0-dev" = "sha256-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM=";
+            # "winit-0.30.8" = "sha256-NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN=";
+            "dpi-0.1.1" = "sha256-hlVhlQ8MmIbNFNr6BM4edKdZbe+ixnPpKm819zauFLQ=";
+          };
 
           inherit nativeBuildInputs;
           inherit buildInputs;
