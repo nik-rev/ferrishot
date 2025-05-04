415692db467c132e496e91f43deb89cb27440137
Nik Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-05-04 18:27:47 +0100

    docs: add changelog entry for version 0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,60 @@
+# v0.3.0 - 4 May 2025
+
+- Added support for touch inputs
+- Ferrishot can now be configured! The config format uses [KDL](https://kdl.dev/). Here is an excerpt from the default config:
+
+```kdl
+// Show the size indicator
+size-indicator #true
+// Show icons around the selection
+selection-icons #true
+
+keys {
+  // Leave the app
+  exit key=<esc>
+
+  // Copies selected region to clipboard, exiting
+  copy-to-clipboard mod=ctrl key=c
+  copy-to-clipboard key=<enter>
+}
+
+theme {
+  // color of the frame around the selection
+  selection-frame 0xab_61_37
+
+  // background color of the region that is not selected
+  non-selected-region 0x00_00_00 opacity=0.5
+}
+```
+
+- Image upload. Press `ctrl + u` to upload your screenshot to the internet. You'll get a preview of the image, link to it and a QR Code.
+- Vim keybindings! Ferrishot is fully controllable via keyboard, if you want to do that
+  - `h | j | k | l` to move by 1 pixel the given side
+  - `H | J | K | L` to extend by 1 pixel the given side
+  - `ctrl + h | j | k | l` shrinks by 1 pixel the given side
+  - Use `alt` in combination with any of the above keys to do the movement of `125px` instead of `1px`
+  - There is also `gg` to move to top-left corner, `G` for bottom-right corner and more. You can view a cheatsheet in-app by pressing `?`.
+  - All the keybindings can be overridden with a custom config. You are also able to define custom ones, if you want it to be `50px` instead of `125px` for all of them simply do `ferrishot --dump-default-config` and then edit the values `125 -> 50`.
+- Super-select mode, press `t` and:
+  - The screen will be divided into 25 regions each assigned a letter. Press any of the letters, then:
+  - The chosen region will be further divided into 25 more regions. This will repeat yet again for a 3rd time.
+  - Allows you to place your cursor into 1 of 15,625 positions on the screen within 4 keystrokes. This will select the top-left corner
+  - Repeat this with the bottom-right corner by using `b`. Now you have selected your screenshot without using a mouse, in just 8 keystrokes!
+- More powerful command line interface
+
+  - `--delay` wait some time before taking a screenshot
+  - `--region` open program with a custom region
+  - `--save-path` choose a file to save the image to, instead of opening file picker (when using `ctrl + s`)
+  - `--accept-on-select` instantly save, upload or copy image to clipboard as soon as you create the first selection.
+
+    This can be cancelled with `ctrl`. It's handy since often you want to instantly copy the selection to clipboard and not do anything fancy.
+
+    So you can therefore set a shell alias like `alias ferrishot=ferrishot --accept-on-select=copy` and that may be satisfying to you 90% of the time.
+
+    In some cases you'd like to save the image, so just hold ctrl to disable this behaviour when releasing the left-mouse button.
+
+    Using this option with `--region` will run ferrishot in "headless mode", without opening a window (as the region created with `--region` _will_ be the first region)
+
 # v0.2.0 - 16 April 2025
 
 - Right-click will snap the closest corner to the current cursor position
--- src/config/cli.rs
+++ src/config/cli.rs
@@ -24,45 +24,59 @@ pub enum AcceptOnSelect {
 #[command(version, about, author = "Nik Revenco")]
 #[expect(clippy::struct_excessive_bools, reason = "normal for CLIs")]
 pub struct Cli {
-    /// (internal) Print markdown of the command line interface
-    #[arg(long, hide = true)]
-    pub markdown_help: bool,
-
     /// Instead of taking a screenshot of the desktop, open this image instead
-    // Currently disabled because if the screenshot is not the same size as the desktop,
-    // it will cause bugs as we consider 0,0 in the Canvas to be the
+    //
+    // NOTE: Currently disabled because if the screenshot is not the same size as the desktop,
+    // it will cause bugs as we consider 0,0 in the Canvas to be the origin but it is not necessarily,
+    // when the desktop and the image are not the same size
+    //
+    // TODO: Fix this argument
+    //
     #[arg(hide = true)]
     pub file: Option<PathBuf>,
 
-    /// Screenshot region to select
+    /// Open ferrishot with a region pre-selected
+    ///
+    /// Using this option with `--accept-on-select` will run ferrishot in 'headless mode',
+    /// without making a new window
     ///
     /// Format: `<width>x<height>+<top-left-x>+<top-left-y>`
-    #[arg(long, value_parser = Rectangle::from_str, value_name = "WxH+X+Y")]
+    #[arg(long, value_parser = Rectangle::from_str, value_name = "WxH+X+Y", verbatim_doc_comment)]
     pub region: Option<Rectangle>,
+    /// Open ferrishot with the last selected region
+    #[arg(long)]
+    pub last_region: bool,
 
-    /// Wait this long before launching
+    /// Accept capture and perform the action as soon as a selection is made
+    ///
+    /// If holding `ctrl` while you are releasing the left mouse button on the first selection,
+    /// the behaviour is cancelled
+    ///
+    /// It's quite useful to run ferrishot, select a region and have it instantly be copied to the
+    /// clipboard for example. In 90% of situations you won't want to do much post-processing of
+    /// the region and this makes that experience twice as fast. You can always opt-out with `ctrl`
+    #[arg(short('a'), long, value_name = "ACTION", verbatim_doc_comment)]
+    pub accept_on_select: Option<AcceptOnSelect>,
+
+    /// Wait this long before launch
     #[arg(
         long,
         value_name = "MILLISECONDS",
         value_parser = |s: &str| s.parse().map(Duration::from_millis),
     )]
     pub delay: Option<Duration>,
+
     /// Instead of opening a file picker to save the screenshot, save it to this path instead
     #[arg(long, value_name = "PATH")]
     pub save_path: Option<PathBuf>,
 
-    /// Accept capture as soon as a selection is made
-    ///
-    /// If holding `ctrl` while you are releasing the left mouse button on the
-    /// first selection, the behaviour is cancelled
-    #[arg(short('a'), long, value_name = "ACTION", verbatim_doc_comment)]
-    pub accept_on_select: Option<AcceptOnSelect>,
-
     //
     // --- Config ---
+    //
     /// Write the default config file
     #[arg(help_heading = "Config", long, help = format!("Write the default config to {}", DEFAULT_CONFIG_FILE_PATH.display()))]
     pub dump_default_config: bool,
+
     /// Specifies the config file to use
     #[arg(
         long,
@@ -73,23 +87,30 @@ pub struct Cli {
     pub config_file: String,
 
     //
-    // --- Logging / Debugging ---
+    // --- Debug ---
+    //
+    // These options are hidden from the user
     //
     /// Choose a minumum level at which to log
-    #[arg(group = "Logging", long, hide = true, default_value_t = log::LevelFilter::Error)]
+    #[arg(help_heading = "Debug", long, hide = true, default_value_t = log::LevelFilter::Error)]
     pub log_level: log::LevelFilter,
     /// Log to stdout instead of file
-    #[arg(group = "Logging", long, hide = true)]
+    #[arg(help_heading = "Debug", long, hide = true)]
     pub log_stdout: bool,
     /// Print the path of the log file
-    #[arg(group = "Logging", long, hide = true, default_value_t = DEFAULT_LOG_FILE_PATH.to_string_lossy().to_string())]
+    #[arg(help_heading = "Debug", long, hide = true, default_value_t = DEFAULT_LOG_FILE_PATH.to_string_lossy().to_string())]
     pub log_file: String,
     /// Launch ferrishot in debug mode (F12)
-    #[arg(long, hide = true)]
+    #[arg(help_heading = "Debug", long, hide = true)]
     pub debug: bool,
     /// Output the path to the log file
-    #[arg(group = "Logging", long, hide = true)]
+    #[arg(help_heading = "Debug", long, hide = true)]
     pub print_log_file_path: bool,
+
+    /// Print markdown of the command line interface
+    /// This looks nicer than just copy-pasting the command line output into a code block
+    #[arg(long, hide = true)]
+    pub markdown_help: bool,
 }
 
 /// Represents the default location of the config file
